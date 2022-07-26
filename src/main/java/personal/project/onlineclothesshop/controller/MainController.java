d2352dd11666826a0041c8ef19996a0311183f95
Hoangdao192 <nguyendanghoangdao2002@gmail.com>
2022-07-26 22:26:54 +0700

    Test git

--- src/main/java/personal/project/onlineclothesshop/controller/MainController.java
+++ src/main/java/personal/project/onlineclothesshop/controller/MainController.java
@@ -12,4 +12,9 @@ public class MainController {
     public String home() {
         return "home";
     }
+
+    @GetMapping(path = {"/about"})
+    public String about() {
+        return "about";
+    }
 }
