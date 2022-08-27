0a093ae7f2a295ffbd9bde33be8037b09ad280e4
Hoangdao192 <nguyendanghoangdao2002@gmail.com>
2022-08-27 16:41:01 +0700

    [React] Complete slide bar component

--- src/main/frontend/src/components/CommonComponent/SlideBar/index.js
+++ src/main/frontend/src/components/CommonComponent/SlideBar/index.js
@@ -4,37 +4,52 @@ import React from "react";
 class SlideBar extends React.Component {
     constructor(props) {
         super(props);
+        this.maxValue = props.maxValue ? 10 : props.maxValue;
+        this.minValue = props.minValue ? 0 : props.minValue;
+
+        this.slideOne = this.slideOne.bind(this);
+        this.slideTwo = this.slideTwo.bind(this);
+        this.fillColor = this.fillColor.bind(this);
+
+        this.firstSliderRef = React.createRef();
+        this.secondSliderRef = React.createRef();
+        this.displayValOneRef = React.createRef();
+        this.displayValTwoRef = React.createRef();
+        this.sliderTrackRef = React.createRef();
+        this.minGap = 0;
+
+        this.numberFormat = new Intl.NumberFormat('de-DE');
+        this.sliderMaxValue = 5000;
     }
 
-    sliderOne = document.getElementById("slider-1");
-    sliderTwo = document.getElementById("slider-2");
-    displayValOne = document.getElementById("range1");
-    displayValTwo = document.getElementById("range2");
-    minGap = 0;
-    sliderTrack = document.querySelector(".slider-track");
-    sliderMaxValue = document.getElementById("slider-1").getAttribute("max");
-    numberFormat = new Intl.NumberFormat('de-DE');
-
     slideOne(){
-        if(parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap){
-            sliderOne.value = parseInt(sliderTwo.value) - minGap;
+        if(parseInt(this.secondSlider.value) - parseInt(this.firstSlider.value) <= this.minGap){
+            this.firstSlider.value = parseInt(this.secondSlider.value) - this.minGap;
         }
-        displayValOne.textContent = (numberFormat.format(sliderOne.value) + 'k');
-        fillColor();
+        this.displayValOne.textContent = (this.numberFormat.format(this.firstSlider.value) + 'k');
+        this.fillColor();
     }
 
     slideTwo(){
-        if(parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap){
-            sliderTwo.value = parseInt(sliderOne.value) + minGap;
+        if(parseInt(this.secondSlider.value) - parseInt(this.firstSlider.value) <= this.minGap){
+            this.secondSlider.value = parseInt(this.firstSlider.value) + this.minGap;
         }
-        displayValTwo.textContent = (numberFormat.format(sliderTwo.value) + 'k');
-        fillColor();
+        this.displayValTwo.textContent = (this.numberFormat.format(this.secondSlider.value) + 'k');
+        this.fillColor();
     }
 
     fillColor(){
-        let percent1 = (sliderOne.value / sliderMaxValue) * 100;
-        let percent2 = (sliderTwo.value / sliderMaxValue) * 100;
-        sliderTrack.style.background = `linear-gradient(to right, #dadae5 ${percent1}% , #3264fe ${percent1}% , #3264fe ${percent2}%, #dadae5 ${percent2}%)`;
+        let percent1 = (this.firstSlider.value / this.sliderMaxValue) * 100;
+        let percent2 = (this.secondSlider.value / this.sliderMaxValue) * 100;
+        this.sliderTrack.style.background = `linear-gradient(to right, #dadae5 ${percent1}% , #3264fe ${percent1}% , #3264fe ${percent2}%, #dadae5 ${percent2}%)`;
+    }
+
+    componentDidMount() {
+        this.firstSlider = this.firstSliderRef.current;
+        this.secondSlider = this.secondSliderRef.current;
+        this.displayValOne = this.displayValOneRef.current;
+        this.displayValTwo = this.displayValTwoRef.current;
+        this.sliderTrack = this.sliderTrackRef.current;
     }
 
     render() {
@@ -42,15 +57,15 @@ class SlideBar extends React.Component {
             <div className={styles.priceFilter}>
                 <p className={styles.filterTitle}>Price</p>
                 <p className={styles.priceRange}>
-                    <span id={styles.range1}>0k</span>
+                    <span ref={this.displayValOneRef} id={styles.range1}>0k</span>
                     -
-                    <span id={styles.range2}>5.000k</span>
+                    <span ref={this.displayValTwoRef} id={styles.range2}>5.000k</span>
                 </p>
-                <div className={[styles.wrapper, styles.rangeSlider].join(' ')}>
+                <div className={styles.wrapper}>
                     <div className={styles.sliderContainer}>
-                        <div className={styles.sliderTrack}></div>
-                        <input type="range" min="0" max="5000" value="0" id="slider-1" onInput="slideOne()"/>
-                        <input type="range" min="0" max="5000" value="5000" id="slider-2" onInput="slideTwo()"/>
+                        <div ref={this.sliderTrackRef} className={styles.sliderTrack}></div>
+                        <input ref={this.firstSliderRef} type="range" min="0" max="5000" defaultValue="0" id="firstSlider" onChange={this.slideOne}/>
+                        <input ref={this.secondSliderRef} type="range" min="0" max="5000" defaultValue="5000" id="secondSlider" onChange={this.slideTwo}/>
                     </div>
                 </div>
             </div>
